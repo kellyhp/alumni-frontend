54cbdd7e65e209e6decc7e8c2a1e82e6991eeb64
Kelly Phan <kelly@Kellys-MacBook-Pro-10.local>
2024-04-10 23:26:23 -0700

    updated navigation responsiveness

--- .next/server/next-font-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/kelly/Desktop/JEKYDashboard/JekyFrontend/app/(pages)/layout\":[\"static/media/c9a5bc6a7c948fb0-s.p.woff2\",\"static/media/a3a474206a856479-s.p.woff2\",\"static/media/e1c529c04de64b40-s.p.woff2\",\"static/media/e5435d1f248a7f69.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
--- app/(pages)/export/page.jsx
+++ app/(pages)/export/page.jsx
@@ -1,6 +1,5 @@
 import Header from "./_components/Header";
 import ExportCard from "./_components/ExportCard";
-import PullCard from "./_components/PullCard";
 import styles from "@components/Layout/Layout.module.scss"
 
 export default function Home() {
@@ -8,9 +7,6 @@ export default function Home() {
     <main className={styles.mains}>
       <Header />
       <ExportCard />
-      <div style={{ marginTop: '2%' }}>
-        <PullCard />
-      </div>
     </main>
   );
 }
